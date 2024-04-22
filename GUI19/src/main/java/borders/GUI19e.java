8a040d229ce74e14aae9efe3e3ecbb290d5e072f
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-21 19:35:31 -0500

    SUNDAY

---
+++ GUI19/src/main/java/borders/GUI19e.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package borders;
+
+import borders.Borderslaw;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUI19e extends JFrame {
+
+    public static void main(String[] args) {
+        Borderslaw layou = new Borderslaw();
+        layou.setVisible(true);
+    }
+}
